2ccf6b379e36d0d80811cfe218f37d0bef3e2b74
juha <mrpenttila@gmail.com>
2026-03-06 09:35:03 +0200

    final2

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -49,7 +49,7 @@ export default function FAQ() {
 
   return (
     <div className="w-full max-w-3xl mx-auto mt-24 px-4 mb-12 text-base md:text-lg" style={{ fontFamily: 'Urbanist, sans-serif' }}>
-      <h2 className="text-5xl font-semibold text-center text-white mb-8" style={{ fontFamily: 'Prata, serif' }}>Usein kysyttyä</h2>
+      <h2 className="text-3xl sm:text-4xl md:text-5xl  font-semibold text-center text-white mb-8" style={{ fontFamily: 'Prata, serif' }}>Usein kysyttyä</h2>
       <div className="flex flex-col gap-4">
         {FAQ_DATA.map((item, index) => (
           <div 
